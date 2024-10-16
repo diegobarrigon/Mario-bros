c7a4e597f2f582c5e76aab57468f21112f3fbd42
diegobarrigon <a23310290@live.ceti.mx>
2024-10-16 00:13:50 +0000

    clase 2

--- include/Porcentaje.hpp
+++ include/Porcentaje.hpp
@@ -1,15 +1,19 @@
+#include <stdexcept>
+
 class Porcentaje
 {
 private:
     int numero;
 public:
-    Porcentaje() {
+    Porcentaje() 
+    {
         this->numero = 0;
     }
-    Porcentaje(int numero){
-        if (numero > 100 || numero < 100)
+    Porcentaje(int numero)
+    {
+        if (numero > 100 || numero < 0)
         {
-                throw new std::exception(2/3)
+                throw new std::runtime_error("Numero fuera de rango");
         }else{
 
             this->numero = numero;
@@ -17,11 +21,18 @@ public:
 
     }
     ~Porcentaje() {}
-    void Incrementar(int numero) {
-
+    void Incrementar(int numero) 
+    {
+        this->numero += numero;
     }
-    void Disminuir(int numero) {
-        
+    void Disminuir(int numero) 
+    {
+        this->numero-= numero;
+    }
+
+    int Imprimir()
+    {
+        return this->numero;
     }
 }
 ;
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include <Porcentaje.hpp>
 
 int main(){
-    std ::cout<<"Hola mundo" <<std::endl;
+    Porcentaje foco3;
+    Porcentaje foco2;
+    Porcentaje foco1;
+
+    foco1.Incrementar(10);
+    foco1.Incrementar(10);
+    foco1.Disminuir(4);
+    std ::cout
+    <<"Foco 1: " 
+    <<foco1.Imprimir()
+    <<std::endl;
 }
