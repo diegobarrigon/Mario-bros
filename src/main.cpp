c9e211626dd79d1956baddc79d37f1ca882f84f4
diegobarrigon <a23310290@live.ceti.mx>
2024-10-18 23:32:16 +0000

    clase 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include <Porcentaje.hpp>
+#include <list>
 
 int main(){
+    std::list<Porcentaje> focos
+
     Porcentaje foco3;
     Porcentaje foco2;
     Porcentaje foco1;
+
+    focos.emplace_back(foco1);
+    focos.emplace_back(foco2);
+    focos.emplace_back(foco3);
+    for (auto &&f : v)
+    {
+        f.Incrementar(10);
+    }
+    
 
     foco1.Incrementar(10);
     foco1.Incrementar(10);
