8b178f933998bd39661b623ad573079caebb7988
diegobarrigon <a23310290@live.ceti.mx>
2024-11-02 14:35:54 +0000

    examen

--- src/prueba.cpp
+++ src/prueba.cpp
@@ -4,36 +4,43 @@
 #include <ftxui/screen/terminal.hpp>
 #include <iostream>
 #include <thread>
+#include <cmath>
 
 using namespace std;
 using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
-    std::string reset_position;
     int frame = 0;
-    auto can = Canvas(90,90);
-    can.DrawPointLine(0,20,0,0);
-    auto ColorSupport = vbox({
-                            Terminal::ColorSupport() >= Terminal::Color::Palette16
-                                ? text(" 16 color palette support : Yes")
-                                : text(" 16 color palette support : No"),
-                            Terminal::ColorSupport() >= Terminal::Color::Palette256
-                                ? text("256 color palette support : Yes")
-                                : text("256 color palette support : No"),
-                            Terminal::ColorSupport() >= Terminal::Color::TrueColor
-                                ? text("       True color support : Yes")
-                                : text("       True color support : No"),
-                        });
+   std::string reseet_position;
+
     while (true)
     {
         Screen pantalla = Screen::Create(Dimension::Full(), Dimension::Full());
-        Element personaje = spinner(20, frame);
-        Element lienzo = bgcolor(Color::Aquamarine1, border(vbox(ColorSupport,personaje,canvas(&can))));
+
+        auto can = Canvas(100,100);
+        can.DrawPointCircle(50,50,30);
+        int ceja = std::sin(frame*0.1)*3;
+        can.DrawPointLine(35,30 - ceja,45,30-ceja);
+        can.DrawPointLine(55,30 - ceja,65,30-ceja);
+        can.DrawPointCircle(40,40,5);
+        can.DrawPointCircle(60,40,5);
+        can.DrawPointCircle(40,40,2);
+        can.DrawPointCircle(60,40,2);
+        int boca = 65 + std::sin(frame*0.1)*3;
+        can.DrawPointLine(35,boca,40,boca+2);
+        can.DrawPointLine(40,boca+2,45,boca+3);
+        can.DrawPointLine(45,boca+3,50,boca+4);
+        can.DrawPointLine(50,boca+4,55,boca+3);
+        can.DrawPointLine(55,boca+3,60,boca+2);
+        can.DrawPointLine(60,boca+2,65,boca);
+
+        Element lienzo = bgcolor(Color::Aquamarine1, canvas(&can));
         Render(pantalla, lienzo);
-        std::cout << reset_position;
+
+        std::cout << reseet_position;
         pantalla.Print();
-        reset_position = pantalla.ResetPosition(true);
+        reseet_position = pantalla.ResetPosition(true);
         this_thread::sleep_for(0.1s);
         frame++;
     }
